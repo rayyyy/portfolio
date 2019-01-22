ed1183c60fade406764630e4a6e127eb7781387f
rayyyy <nraavy@gmail.com>
2019-01-22 22:06:15 +0900

    トップコンポーネント追加

---
+++ src/app/pages/top/top.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top',
+  templateUrl: './top.component.html',
+  styleUrls: ['./top.component.sass']
+})
+export class TopComponent implements OnInit {
+
+  constructor() {
+    console.log(1122211);
+  }
+
+  ngOnInit() {
+  }
+
+}
